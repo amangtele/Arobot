b59b5f3bfde45db44f271c0cc61480fb76458b66
amanqs <116822442+amanqs@users.noreply.github.com>
2023-03-05 11:33:30 +0700

    Create MusicCommand.py

---
+++ wbb/modules/MusicCommand.py
@@ -0,0 +1,31 @@
+__MODULE__ = "MUSIC COMMAND"
+__HELP__ = """
+✅Play Commands:
+cplay or cstream stands for channel play.
+vplay stands for video play.
+
+• /play or /vplay or /cplay  - Bot will start playing your given query on voice chat.
+• /stream or /cstream - Stream live links on voice chats.
+• /channelplay Chat username or id or Disable - Connect channel to a group and stream music on channel's voice chat from your group.
+
+✅Bot's Server Playlists:
+• /playlist  - Check Your Saved Playlist On Servers.
+• /deleteplaylist - Delete any saved music in your playlist
+• /play  - Start playing Your Saved Playlist from Servers.
+
+✅Admin Commands:
+c stands for channel play.
+• /pause or /cpause - Pause the playing music.
+• /resume or /cresume- Resume the paused music.
+• /mute or /cmute- Mute the playing music.
+• /unmute or /cunmute- Unmute the muted music.
+• /skip or /cskip- Skip the current playing music.
+• /stop or /cstop- Stop the playing music.
+• /shuffle or /cshuffle- Randomly shuffles the queued playlist.
+
+✅Specific Skip:
+• /skip or /cskip Number(example: 3) 
+
+✅Loop Play:
+• /loop or /cloop enable/disable or Numbers between 1-10 - When activated
+"""
